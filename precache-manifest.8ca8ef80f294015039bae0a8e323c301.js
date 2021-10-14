28f70d70ed8edf887f9a9eb5c80f09198d6be07c
Gorokhovik Dmitry <dima_lgtu@mail.ru>
2021-10-14 17:01:11 +0300

    Initial dist subtree commit

---
+++ precache-manifest.8ca8ef80f294015039bae0a8e323c301.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f52d1fccfcc90bd030",
+    "url": "/css/app.35584a51.css"
+  },
+  {
+    "revision": "4d7eccce54993876b89a",
+    "url": "/css/chunk-vendors.6ac4ec36.css"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/fonts/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "781190aecb862fffe858d42b124658cc",
+    "url": "/fonts/Montserrat-BoldItalic.781190ae.ttf"
+  },
+  {
+    "revision": "6786546363c0261228fd66d68bbf27e9",
+    "url": "/fonts/Montserrat-Italic.67865463.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/fonts/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "428b2306e9c7444556058c70822d7d7c",
+    "url": "/fonts/Montserrat-LightItalic.428b2306.ttf"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/fonts/Montserrat-Medium.a98626e1.ttf"
+  },
+  {
+    "revision": "287208c81e03eaf08da630e1b04d80e8",
+    "url": "/fonts/Montserrat-MediumItalic.287208c8.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/fonts/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/fonts/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "a9fd17feca88690cda4bf3b393ca5451",
+    "url": "/img/class1.a9fd17fe.png"
+  },
+  {
+    "revision": "c588f5270fe78bf35e6e476ed062ce8f",
+    "url": "/img/class2.c588f527.png"
+  },
+  {
+    "revision": "bce994ba319eff70fd817713592769e6",
+    "url": "/img/class3.bce994ba.png"
+  },
+  {
+    "revision": "e0ac232faf133572e6856f7ef595425e",
+    "url": "/img/logo.e0ac232f.svg"
+  },
+  {
+    "revision": "aa73a6e1f95d0ba8648c679c5d2e37e5",
+    "url": "/img/portf1.aa73a6e1.png"
+  },
+  {
+    "revision": "fec7517b7157d723a8244b2adaaf41fe",
+    "url": "/img/portf2.fec7517b.png"
+  },
+  {
+    "revision": "a2847ea16c3b6b39e504a65c5f973c24",
+    "url": "/img/portf3.a2847ea1.png"
+  },
+  {
+    "revision": "bd673fc3c135765233d2d328cb8dac42",
+    "url": "/img/teacher.bd673fc3.png"
+  },
+  {
+    "revision": "8a18dfd4f8f0c2819ae6b7fe55ff0252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3f52d1fccfcc90bd030",
+    "url": "/js/app.455b349e.js"
+  },
+  {
+    "revision": "4d7eccce54993876b89a",
+    "url": "/js/chunk-vendors.8abfecea.js"
+  },
+  {
+    "revision": "d514c32abe8d20075368704d46759a9d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
